249219b4af5c61d0091828ce7c894d592cf17e00
Michalek <michalek@m-cloud.cz>
2021-05-07 20:02:28 +0200

    Configurable volumes script

--- files/opt/bareos_utils/leftover_volumes.sh
+++ files/opt/bareos_utils/leftover_volumes.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-VOLUMESDB=$(runuser -u postgres -- psql bareos -t -c "select volumename from media where volumename like 'Full-%' order by volumename asc")
-VOLUMESFS=$(find /var/lib/bareos/storage/ -name "Full-*" -mindepth 1 -maxdepth 1 -type f -printf '%P\n' | sort)
+VOLUME_TYPE=Full
+VOLUME_PATH=/var/lib/bareos/storage
+VOLUMESDB=$(runuser -u postgres -- psql bareos -t -c "select volumename from media where volumename like '${VOLUME_TYPE}-%' order by volumename asc")
+VOLUMESFS=$(find "${VOLUME_PATH}" -mindepth 1 -maxdepth 1 -name "${VOLUME_TYPE}-*" -type f -printf '%P\n' | sort)
 for volumefs in ${VOLUMESFS}; do
   if [[ ! " ${VOLUMESDB[*]} " =~ ${volumefs} ]]; then
     echo "${volumefs} exists on disk, but not found in DB"
