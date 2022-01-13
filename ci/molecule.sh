6e1b628dd59fa61ef76dc684beed8552052bd66e
Michalek <michalek@m-cloud.cz>
2022-01-13 13:46:08 +0100

    Molecule local CI update

--- ci/molecule.sh
+++ ci/molecule.sh
@@ -2,13 +2,15 @@
 docker \
   run \
   --rm \
+  -ti \
   -v "$(pwd):/tmp/$(basename "${PWD}")" \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -w "/tmp/$(basename "${PWD}")" \
-  -e MOLECULE_DISTRO_SERVER \
-  -e MOLECULE_DISTRO_CLIENT \
+  -e MOLECULE_IMAGE_SERVER \
+  -e MOLECULE_IMAGE_CLIENT \
   -e MOLECULE_NO_LOG=false \
   veselahouba/molecule bash -c "
+  shellcheck_wrapper && \
   flake8 && \
   yamllint . && \
   ansible-lint && \
